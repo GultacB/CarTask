a99fa2d688cd16d53942e23e70f8b7b0ff2549e0
GultacB <160045911+GultacB@users.noreply.github.com>
2024-03-06 11:50:11 +0400

    CarTask

---
+++ CarTask/CarTask/CarProperty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTask
+{
+    public class CarProperty
+    { public string Brands { get; set; }
+      public int Models { get; set; }
+      public string Colour { get; set; }
+      public long ProductionYear{ get; set; }
+      public string GenerationType {  get; set; }
+
+
+    }
+}
---
+++ CarTask/CarTask/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using CarTask;
+Console.WriteLine("Brands");
+string brands = Console.ReadLine();
+Console.WriteLine("Models");
+int models = int.Parse(Console.ReadLine());
+Console.WriteLine("Colour");
+string colour = Console.ReadLine();
+Console.WriteLine("Production Year");
+long productionYear = long.Parse(Console.ReadLine());
+Console.WriteLine("Generation Type");
+string generationType = Console.ReadLine();
+List<CarProperty> Car = new List<CarProperty>()
+{
+new CarProperty(){ Brands=brands, Models=models, Colour=colour, ProductionYear=productionYear, GenerationType=generationType },
+  
+};
+foreach (var CarProperty in Car)
+{
+    Console.WriteLine($"Car Proporties=>Brands:{CarProperty.Brands} Models:{CarProperty.Models} Colour:{CarProperty.Colour} Production Year:{CarProperty.ProductionYear} Generation Type:{CarProperty}");
+}
+
+
